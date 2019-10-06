f9f1c2aef9aa59883db39feb9dc845981aef895e
Sean <shonwr@gmail.com>
2019-10-05 20:14:08 -0700

    First commit

---
+++ Hello World/Hello World/Hello World.cpp
@@ -0,0 +1,16 @@
+// Hello World.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World";
+
+	getchar();
+    return 0;
+}
+
